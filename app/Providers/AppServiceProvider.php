8f936b9e04660801c2352147071032e7993f11a0
FariazKhan <50790572+FariazKhan@users.noreply.github.com>
2022-06-24 21:21:45 +0600

    Fixed Missing App\Providers\Schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\BlogSettings;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
